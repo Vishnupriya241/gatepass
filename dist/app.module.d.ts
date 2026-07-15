8f82ca1a06e2979935da207285b7eff26f2a5803
Vishnupriya241 <72873668+Vishnupriya241@users.noreply.github.com>
2026-07-15 22:46:55 +0530

    Delete dist directory

--- dist/app.module.d.ts
+++
@@ -1,3 +0,0 @@
-import './database';
-export declare class AppModule {
-}
--- dist/database.d.ts
+++
@@ -1,9 +0,0 @@
-interface GatepassSummaryRow {
-    approved: number;
-    pending: number;
-    rejected: number;
-    name: string;
-    role: string;
-}
-export declare const database: any;
-export type { GatepassSummaryRow };
